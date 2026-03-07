631cf4a04f77a2dd0984f1790dfcba1c22b1b277
dkks112313 <ovcharenkoden05@gmail.com>
2026-03-07 23:01:50 +0200

    Revert "Test"
    This reverts commit 3dfa39e0aa8da66366895fa1b8f5d61629df9dbf.

--- Program.cs
+++ Program.cs
@@ -11,8 +11,7 @@ namespace WinFormsApp8
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());      
-            // TEST
+            Application.Run(new Form1());
         }
     }
 }
