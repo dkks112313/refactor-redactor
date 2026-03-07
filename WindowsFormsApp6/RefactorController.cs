3ff1cb53f1648229e2b3f981bc0c299190a18301
s01nus <ig.solodpl@gmail.com>
2026-03-08 00:16:22 +0200

    Added code for Lab1

---
+++ WindowsFormsApp6/RefactorController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp6
+{
+    public class RefactorController
+    {
+        public string RemoveParameter(string CodeText, string methodName, string parameter)
+        {
+            return "";
+        }
+    }
+}
