a2a72d36a6ba9ae57d5a0380ae968bafbc8698c3
dkks112313 <ovcharenkoden05@gmail.com>
2026-03-08 19:02:39 +0200

    tests

---
+++ UnitTest/ConstTests/ConstTest.cs
@@ -0,0 +1,63 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest.ConstTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod3()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod4()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod5()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+        }
+    }
+}
