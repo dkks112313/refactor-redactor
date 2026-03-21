1692a393abf7989acba0b32ef273d803f798d0cc
Agent3141592 <polinaforlerning@ukr.net>
2026-03-21 23:02:23 +0200

    Revert "refactor method"
    This reverts commit 7d6c989d11be311f2ef34cafbe8aebc21e2d62c3.

---
+++ RefactorApp/RefactorRenameMethodController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp6
+{
+    internal class RefactorRenameMethodController
+    {
+        public string RenameMethod(string nameMethod, string newNameMethod, string _empty)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
